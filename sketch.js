dfda435bc3411ef1801d2bad76103b64ae754310
PaulaGabriela <141455050+PaulaGabriela01@users.noreply.github.com>
2023-08-11 18:00:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+let tamanho;
+
+function setup() {
+  createCanvas(600, 600);
+  background("pink");
+  cor=color(random(0,255),random(0,255),random(0,255))
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+  tamanho = 60;
+}
+
+
+function draw() {
+  fill(cor)
+  circle(posicaoHorizontal, posicaoVertical, tamanho );
+  if(mouseX< posicaoHorizontal){
+     posicaoHorizontal -=1
+  }
+   if (mouseX> posicaoHorizontal) {
+     posicaoHorizontal +=1
+  }
+  if(mouseY > posicaoVertical){
+    posicaoVertical += 1
+  }
+  if (mouseY < posicaoVertical){
+    posicaoVertical -= 1
+  }
+  if (mouseIsPressed){
+    cor=color(random(0,255),random(0,255),random(0,255),random(0,100))
+  }
+  }
